d07f0e471c458116662790da3c3e7c563db27924
mad-11 <mad-11@mad-11s-macbook-pro.local>
2019-04-23 19:31:06 +0200

    Initial Commit

---
+++ RediSudoku/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RediSudoku
+//
+//  Created by mad-11 on 23.04.19.
+//  Copyright © 2019 RaDI School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
